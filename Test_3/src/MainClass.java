a1dd651deca9a3749e8786925211f77b873a927f
AntonAuto <54001579+AntonAuto@users.noreply.github.com>
2019-08-11 21:44:16 +0500

    Add files via upload
    Home work Test_3 a question about java type

---
+++ Test_3/src/MainClass.java
@@ -0,0 +1,14 @@
+public class MainClass
+{
+  //  private int class_string (String text)
+  //  {
+  //      System.out.println(text);
+  //      return 0;
+  //  }
+    private int class_string = "Hello, World";
+
+    public int getClassString()
+    {
+        return class_string;
+    }
+}
---
+++ Test_3/src/MainClassTest.java
@@ -0,0 +1,17 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainClassTest extends MainClass
+{
+//    @Before
+ //   public void textStartTest()
+ //   {
+ //       System.out.println("A test is starting");
+ //   }
+    @Test
+    public void testGetClassString()
+    {
+        int a = this.getClassString();
+    }
+}
