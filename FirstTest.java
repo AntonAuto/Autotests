6aacd5110c00a01da4f4debe95c7d54c30e5ff99
AntonAuto <54001579+AntonAuto@users.noreply.github.com>
2019-09-09 08:28:30 +0300

    Add files via upload
    draft for the lesson 2 excersice 4 (all results containt Java word)

---
+++ FirstTest.java
@@ -0,0 +1,56 @@
+import lib.CoreTestCase;
+import lib.ui.*;
+import org.junit.Test;
+
+public class FirstTest extends CoreTestCase {
+
+
+    // For exercise 4 Lesson#2
+//    @Test
+//    public void test123(){
+//        waitForElementAndClick(
+//                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+//                "Cannot find Search Wikipedia Input",
+//                5
+//        );
+//        waitForElementAndSendKeys(
+//                By.xpath("//*[contains(@text,'Search…')]"),
+//                "Java",
+//                "Cannot find search input",
+//                5
+//        );
+//        String search_result_locator = "//*[@resource-id='org.wikipedia:id/search_results_list']/*[@resource-id='org.wikipedia:id/page_list_item_container']";
+//        waitForElementPresent(
+//                By.xpath(search_result_locator),
+//                "Cannot find any topics searching by 'Java'",
+//                15
+//        );
+//        int amount_of_articles = getAmountOfElements(By.xpath(search_result_locator));
+//        int index_number = 0;
+//        List<String> aamounts = new ArrayList<String>();
+//        for (int i=0; i<amount_of_articles;i++) {
+//            aamounts.add(index_number,waitForElementAndGetAttribute(
+//                    By.id("org.wikipedia:id/page_list_item_title"),
+//                    "text",
+//                    "Cannot add title of the article",
+//                    10
+//            ));
+//            index_number = index_number + 1;
+//        }
+//        System.out.println(aamounts);
+
+//        for (int i=0; i<amount_of_articles;i++){
+
+//        List aamonts = Collections.singletonList(waitForElementAndGetAttribute(
+//                By.id("org.wikipedia:id/page_list_item_title"),
+//                "text",
+//                "cannot find the title for articles in search results",
+//                10
+//        )
+//        );
+ //       System.out.println(amount_of_articles);
+//        System.out.println(aamonts);
+//
+//    }
+
+        }
