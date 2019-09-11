fc321e7cfa42f14976ffbc673c80f0d652000e86
AntonAuto <54001579+AntonAuto@users.noreply.github.com>
2019-09-11 08:29:59 +0300

    Add files via upload
    IOS platform tests

---
+++ JavaAppiumAutomation/src/tests/iOS/GetStartedTest.java
@@ -0,0 +1,12 @@
+package tests.iOS;
+
+import lib.iOSTestCase;
+import org.junit.Test;
+
+public class GetStartedTest extends iOSTestCase {
+
+    @Test
+    public void testPassThroughWelcome(){
+
+    }
+}
